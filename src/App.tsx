32be65a19b3f862da51f96941a91de489e2bc112
Jiali Zhang <zhangjiali86@gmail.com>
2023-03-06 14:49:13 +0100

    finish UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="grid grid-template">
+      <div className="col-span-full bg-black bg-opacity-75 flex flex-col justify-around items-end p-3 break-all">
+        <div className="text-white text-opacity-75"></div>
+        <div className="text-white text-xl"></div>
+      </div>
+      <button className="col-span-2 text-xl border border-white bg-white bg-opacity-50 hover-focus">AC</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">DEL</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">÷</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">1</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">2</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">3</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">*</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">4</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">5</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">6</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">+</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">7</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">8</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">9</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">-</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">.</button>
+      <button className=" text-xl border border-white bg-white bg-opacity-50 hover-focus">0</button>
+      <button className="col-span-2 text-xl border border-white bg-white bg-opacity-50 hover-focus">=</button>
     </div>
   );
 }
