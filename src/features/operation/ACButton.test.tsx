e91fb24da78c70c0c12c04008fcc81ddcf093fc9
Jiali Zhang <zhangjiali86@gmail.com>
2023-03-24 10:41:24 +0100

    update readme

---
+++ src/features/operation/ACButton.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react"
+import ACButton from "./ACButton"
+
+// describe('ACButton',()=>{
+//     test('renders correctly', ()=>{
+//         render(<ACButton/>)
+//         const acButton = screen.get
+//     })
+// })
